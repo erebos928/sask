e28c87aa9a4e11fce3ea6aab763584e173b81ed1
Behrooz Shahin <shahin.behrooz@gmail.com>
2023-08-01 17:14:13 -0600

    mold commit

--- demo/src/main/java/sask/demo/DemoApplication.java
+++ demo/src/main/java/sask/demo/DemoApplication.java
@@ -2,12 +2,15 @@ package sask.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ * hgfhgfgfgf
+ */
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		//System
 	}
 
 }
